7833b3947c016337af72468a87f615c872f8bb14
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-11-16 04:22:13 -0500

    Update deploy-heroku.sh

--- DevOps/deploy-heroku.sh
+++ DevOps/deploy-heroku.sh
@@ -1,9 +1,4 @@
-#!/bin/bash -e
-# Copyright 2018 Google Inc. Use of this source code is governed by an MIT-style
-# license that can be found in the LICENSE file or at
-# https://opensource.org/licenses/MIT.
-
-curl https://kolkrabbi.heroku.com/apps/98fc74a8-ff56-4a21-85f6-7a1fc8ba95c9/github/push \
+-curl https://kolkrabbi.heroku.com/apps/98fc74a8-ff56-4a21-85f6-7a1fc8ba95c9/github/push \
   -H "Content-Type: application/json" \
   -H "Accept: application/json, text/javascript, */*; q=0.01" \
   -H "Authorization: Bearer $HEROKU_TOKEN" \
